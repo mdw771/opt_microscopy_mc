c9185f75d8e89e933f6e69fc9915b75fa187bc1d
Ming <mingdu2015@u.northwestern.edu>
2016-10-19 09:34:44 -0500

    photon class

--- main.m
+++ main.m
@@ -1,4 +1,68 @@
+%% 
+% Lists for refractive indices and layer boundary positions follow the
+% following format:
+% n = [amb.n, n1, n2, ..., amb.n];
+% z = [0, z1, z2, z3, ..., z_n].
+% Layers are indiced in such a way that layer 1 corresponds to the upper
+% ambience, and the last index corresponds to the lower ambience, i.e., the
+% number of layer indices is equal to the length of refractive index list.
+%%
+% set parameters
 clear;
-ph = photon();
-ph = ph.init();
-ph = ph.scatter(0.5);
+n_ls = [1, 1, 1];
+z_ls = [0, 1];
+mu_a_ls = [0, 0.1, 0];
+mu_s_ls = [0, 100, 0];
+g_ls = [1, 0.9, 1];
+mu_t_ls = mu_a_ls + mu_s_ls;
+n_photon = 1000;
+w_thresh = 0.0001;
+m = 10;
+
+x=[];
+y=[];
+z=[];
+w=[];
+
+% start loop
+for i_photon = 1:n_photon
+    i_photon
+    % initialize
+    ph = photon();
+    ph = ph.initialize(0, 0);
+
+    while ~(ph.dead)
+        
+        % set new s if s = 0
+        if ph.s == 0
+            ph = ph.get_s();
+        end
+
+        % check boundary and move
+        ph = ph.move(z_ls, mu_t_ls);
+
+        % if no booundary hit, absorb and scatter
+        if ph.s == 0
+            ph = ph.absorb(mu_a_ls, mu_s_ls);
+            ph = ph.scatter(g_ls);
+            %
+            x(i_photon, ph.scatters) = ph.x;
+            y(i_photon, ph.scatters) = ph.y;
+            z(i_photon, ph.scatters) = ph.z;
+            w(i_photon, ph.scatters) = ph.w;
+            %
+        % if boundary hit, transmit or reflect
+        else
+            ph = ph.reflect_transmit(n_ls);
+        end
+
+        % if photon is alive but low in weight, run roulette
+        if ~(ph.dead)
+            if ph.w < w_thresh
+                ph = ph.terminate(m);
+            end
+        end
+        
+    end
+        
+end
--- photon.m
+++ photon.m
@@ -9,10 +9,10 @@ classdef photon
         layer
     end
     methods
-        function obj = init(obj)
+        function obj = initialize(obj, x, y)
             % initialize photon
-            obj.x = 0;
-            obj.y = 0;
+            obj.x = x;
+            obj.y = y;
             obj.z = 0;
             obj.ux = 0;
             obj.uy = 0;
@@ -21,21 +21,23 @@ classdef photon
             obj.dead = false;
             obj.s = 0;
             obj.scatters = 0;
-            obj.layer = 1;
+            obj.layer = 2;
         end
         function obj = get_s(obj)
             % get random s
             xi = time_rand();
             obj.s = -log(xi);
         end
-        function [obj, delta_w] = absorb(obj, mu_ratio)
+        function [obj, delta_w] = absorb(obj, mu_a_ls, mu_t_ls)
             % deduce weight due to absorption
+            mu_ratio = mu_a_ls(obj.layer)/mu_t_ls(obj.layer);
             delta_w = obj.w * mu_ratio;
             obj.w = obj.w - delta_w;
         end
-        function obj = scatter(obj, g)
+        function obj = scatter(obj, g_ls)
             % update direction cosines upon scattering
             xi = time_rand();
+            g = g_ls(obj.layer);
             if g ~= 0
                 cosine = 1 + g^2 - ((1-g^2)/(1-g+2*g*xi))^2;
                 cosine = cosine / (2*g);
@@ -59,9 +61,12 @@ classdef photon
                 obj.uy = sin(theta) * sin(phi);
                 obj.uz = sign(muz) * cosine;
             end
+            obj.scatters = obj.scatters + 1;
         end
-        function obj = check_boundary(obj, z_bound)
-            % modify s if photon will hit boundary in the next move
+        function obj = move(obj, z_ls, mu_t_ls)
+            % judge if the photon will hit boundary then move it 
+            mu_t = mu_t_ls(obj.layer);
+            z_bound = [z_ls(obj.layer-1), z_ls(obj.layer)];
             if obj.uz < 0
                 db = (z_bound(1)-obj.z) / obj.uz;
             elseif obj.uz == 0
@@ -69,7 +74,67 @@ classdef photon
             else
                 db = (z_bound(2)-obj.z) / obj.uz;
             end
-            error('NotImplemented')
+            if db*mu_t <= obj.s
+                % hit boundary
+                obj.x = obj.x + obj.ux*db;
+                obj.y = obj.y + obj.uy*db;
+                obj.z = obj.z + obj.uz*db;
+                obj.s = obj.s - db*mu_t;
+            else
+                % does not hit boundary
+                obj.x = obj.x + obj.ux*obj.s/mu_t;
+                obj.y = obj.y + obj.uy*obj.s/mu_t;
+                obj.z = obj.z + obj.uz*obj.s/mu_t;
+                obj.s = 0;
+            end
+        end
+        function obj = reflect_transmit(obj, n_ls)
+            % do reflection or transmission
+            dir = 0;
+            a_i = acos(abs(obj.uz));
+            n_i = n_ls(obj.layer);
+            if obj.uz < 0
+                n_t = n_ls(obj.layer-1);
+                dir = -1;
+            else
+                n_t = n_ls(obj.layer+1);
+                dir = 1;
+            end
+            a_t = asin(n_i*sin(a_i)/n_t);
+            if n_i > n_t && a_i > asin(n_t/n_i)
+                r = 1;
+            else
+                r = (sin(a_i-a_t)/sin(a_i+a_t))^2 ;
+                r = 0.5 * (r+(tan(a_i-a_t)/tan(a_i+a_t))^2);
+            end
+            xi = time_rand();
+            if xi <= r
+                % internally reflected
+                obj.uz = -obj.uz;
+            else
+                % transmitted
+                if dir == -1
+                    obj.layer = obj.layer - 1;
+                else
+                    obj.layer = obj.layer + 1;
+                end
+                if obj.layer == 1 || obj.layer == length(n_ls)
+                    obj.dead = true;
+                else
+                    obj.ux = obj.ux*n_i/n_t;
+                    obj.uy = obj.uy*n_i/n_t;
+                    obj.uz = sign(obj.uz)*cos(a_t);
+                end
+            end
+        end
+        function obj = terminate(obj, m)
+            xi = time_rand();
+            if xi <= 1/m
+                obj.w = m*obj.w;
+            else
+                obj.w = 0;
+                obj.dead = true;
+            end
         end
     end
 end
